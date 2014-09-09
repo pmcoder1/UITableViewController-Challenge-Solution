e9fa8f88d36731471715463fdd72bbdd95b80d8c
Ron Duran <ronduran@Ron-Durans-MacBook-Pro.local>
2014-09-08 22:23:28 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/RDDViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDDViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ron Duran on 9/8/14.
+//  Copyright (c) 2014 RDuran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDDViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/RDDViewController.m
@@ -0,0 +1,29 @@
+//
+//  RDDViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Ron Duran on 9/8/14.
+//  Copyright (c) 2014 RDuran. All rights reserved.
+//
+
+#import "RDDViewController.h"
+
+@interface RDDViewController ()
+
+@end
+
+@implementation RDDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
